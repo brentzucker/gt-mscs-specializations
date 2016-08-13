91bb707b4ecd6e89cce6f84f2d7b7cf00b0aac0f
brentzucker <brentzucker@gmail.com>
2016-08-13 13:26:30 -0400

    print contents of each of the specialization tables

--- scrape.py
+++ scrape.py
@@ -11,5 +11,17 @@ from bs4 import BeautifulSoup
 def scrape():
 	url = 'http://www.cc.gatech.edu/academics/degree-programs/masters/computer-science/specializations'
 	
+	page_html = requests.get(url).text
+
+	soup = BeautifulSoup(page_html, 'html.parser')
+
+	tables = soup.find_all('table')
+
+	# 12 tables on page, but only 11 specializations
+	del tables[11]
+
+	for table in tables:
+		print table.text
+
 if __name__ == '__main__':
 	scrape()
