e357d586b66110e070115dd54c386b87a35a3a68
brentzucker <brentzucker@gmail.com>
2016-08-13 13:18:45 -0400

    initial commit

---
+++ scrape.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python 2.7.10
+# pip install requests
+# pip install BeautifulSoup4
+
+import requests
+from bs4 import BeautifulSoup
+
+def scrape():
+	url = 'http://www.cc.gatech.edu/academics/degree-programs/masters/computer-science/specializations'
+	
+if __name__ == '__main__':
+	scrape()
